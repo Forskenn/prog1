b1e72bc50223945ea54f267ee6cd40f6af958bc8
Marlon Rene Rivas <mrfo1925@hotmail.com>
2018-08-27 18:26:36 -0500

    New msg

--- Hola.java
+++ Hola.java
@@ -3,5 +3,6 @@ public class Hola{
                         public static void main (String args []){
                      
                              system.out.printl("Que tranza carnal"):
+                      system.out.printl("Que onda"):
 }
 }
